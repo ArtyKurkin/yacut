8bf0e03b64c7a9adf348660ded7433691c2f5d3a
Артём Куркин <artykn88@gmail.com>
2023-03-21 12:44:17 +0300

    for the first review

---
+++ yacut/api_views.py
@@ -0,0 +1,38 @@
+from http import HTTPStatus
+from re import match
+
+from flask import jsonify, request
+
+from . import app, db
+from .error_handlers import InvalidAPIUsage
+from .models import URLMap
+from .utils import get_unique_short_id
+
+
+@app.route('/api/id/', methods=['POST'])
+def add_url():
+    pattern_short = r'^[A-Za-z0-9_]{1,16}$'
+    data = request.get_json()
+    if data is None:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+    if 'url' not in data:
+        raise InvalidAPIUsage('"url" является обязательным полем!')
+    if not data.get('custom_id'):
+        data['custom_id'] = get_unique_short_id()
+    if not match(pattern_short, data['custom_id']):
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+    if URLMap.query.filter_by(short=data['custom_id']).first():
+        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
+    url_map = URLMap()
+    url_map.from_dict(data)
+    db.session.add(url_map)
+    db.session.commit()
+    return jsonify(url_map.to_dict()), HTTPStatus.CREATED
+
+
+@app.route('/api/id/<string:short_id>/', methods=['GET'])
+def get_url(short_id):
+    url = URLMap.query.filter_by(short=short_id).first()
+    if not url:
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+    return jsonify({'url': url.original}), HTTPStatus.OK
---
+++ yacut/views.py
@@ -0,0 +1,41 @@
+from flask import flash, redirect, render_template, request, url_for
+
+from . import app, db
+from .forms import URLMapForm
+from .models import URLMap
+from .utils import get_unique_short_id
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index_view():
+    form = URLMapForm()
+    if form.validate_on_submit():
+        original_link = form.original_link.data
+        custom_id = form.custom_id.data
+        if custom_id and URLMap.query.filter_by(short=custom_id).first() is not None:
+            flash(f'Имя {custom_id} уже занято!')
+            return redirect(url_for('index_view'))
+
+        if not original_link:
+            flash('Введите оригинальную ссылку')
+            return redirect(url_for('index_view'))
+
+        if not custom_id:
+            custom_id = get_unique_short_id()
+
+        new_link = URLMap(
+            original=form.original_link.data,
+            short=custom_id
+        )
+        db.session.add(new_link)
+        db.session.commit()
+        short_url = f'{request.host_url}{custom_id}'
+        return render_template('index.html', form=form, short_url=short_url)
+    return render_template('index.html', form=form)
+
+
+@app.route('/<short>')
+def redirect_link(short):
+    return redirect(
+        URLMap.query.filter_by(short=short).first_or_404().original
+    )
